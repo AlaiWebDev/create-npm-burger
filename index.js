790d962773ecc2248bbbe76812b5c9b2fd89dacb
Alain Orluk <alainwebdev@free.fr>
2023-07-26 00:43:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const getRepos = async ({
+    username = 'alaiwebdev',
+    page = 1,
+    per_page = 30
+} = {}) => {
+    try {
+        const repos = await axios.get()
+    }
+}
